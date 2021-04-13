e9b43643e73bc42b862f2ab29c60cda067e7fcb1
By_Ha <2051261534@qq.com>
2021-04-13 23:12:18 +0800

    fix: wish auto update database operation error

--- src/utils/wish_auto_update.js
+++ src/utils/wish_auto_update.js
@@ -1,8 +1,14 @@
 const { getGachaList, getGachaDetail } = require('./api')
-const { push } = require('./database');
+const { update, get, push } = require('./database');
 
 exports.wishUpdate = async () => {
     const list = JSON.parse(await getGachaList()).data.list;
     const detail = JSON.parse(await getGachaDetail(list[1].gacha_id));
-    push('wish', 'wishes', detail);
+    if (await get('wish', 'wishes', { gacha_type: list[1].gacha_type.toString() })) {
+        update('wish', 'wishes', { gacha_type: list[1].gacha_type.toString() }, detail);
+    } else {
+        push('wish', 'wishes', detail);
+    }
 }
+
+this.wishUpdate();
