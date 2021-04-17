29f4d66ae0c00da92402784912ead3cf976630c6
SilveryStar <1104247794@qq.com>
2021-04-18 03:35:27 +0800

    fix: save id error

--- src/plugins/save/index.js
+++ src/plugins/save/index.js
@@ -1,6 +1,6 @@
 const { isInside, push, update } = require('../../utils/database');
 
-module.exports = Message => {
+module.exports = async Message => {
     let msg = Message.raw_message;
     let userID = Message.user_id;
     let groupID = Message.group_id;
@@ -11,18 +11,18 @@ module.exports = Message => {
     } else {
         mhyID = parseInt(id[0]);
         if (msg.includes('#s') || msg.includes('绑定')) {
-            if (!isInside('map', 'user', 'userID', userID)) {
-                push('map', 'user', {userID, mhyID});
-                if (!isInside('time', 'user', 'mhyID', mhyID)) {
-                    push('time', 'user', {mhyID, time: 0});
+            if (!(await isInside('map', 'user', 'userID', userID))) {
+                await push('map', 'user', {userID, mhyID});
+                if (!(await isInside('time', 'user', 'mhyID', mhyID))) {
+                    await push('time', 'user', {mhyID, time: 0});
                 }
                 bot.sendGroupMsg(groupID, "通行证绑定成功，使用 #gq 来查询游戏信息").then();
             } else {
                 bot.sendGroupMsg(groupID, "您已绑定通行证，请使用 #c " + mhyID).then();
             }
         } else if (msg.includes('#c') || msg.includes('改绑')) {
-            if (isInside('map', 'user', 'userID', userID)) {
-                update('map', 'user', {userID}, {mhyID});
+            if (await isInside('map', 'user', 'userID', userID)) {
+                await update('map', 'user', {userID}, {mhyID});
                 bot.sendGroupMsg(groupID, "通行证改绑成功").then();
             } else {
                 bot.sendGroupMsg(groupID, "您还未绑定通行证，请使用 #s " + mhyID).then();
