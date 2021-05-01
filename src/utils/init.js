c31ea75c5d86c92bbf7ccf686dc8ee274edb6da8
SilveryStar <1104247794@qq.com>
2021-05-02 00:26:16 +0800

    pref: optimize image render speed

--- src/utils/init.js
+++ src/utils/init.js
@@ -2,6 +2,7 @@ const { newDB } = require('./database');
 const { newServer } = require('./server');
 const { gachaUpdate } = require('./update')
 const schedule = require('node-schedule');
+const puppeteer = require('puppeteer')
 
 const databaseInitialize = () => {
     newDB('map');
@@ -13,7 +14,7 @@ const databaseInitialize = () => {
     newDB('gacha', { user: [], data: [] });
 };
 
-module.exports = () => {
+module.exports = async () => {
     newServer(9934);
     databaseInitialize();
     gachaUpdate();
@@ -23,5 +24,10 @@ module.exports = () => {
     });
     schedule.scheduleJob('0 1 18 * * *', () => {
         gachaUpdate();
+    });
+
+    global.browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 }
--- src/utils/render.js
+++ src/utils/render.js
@@ -1,22 +1,15 @@
-const puppeteer = require('puppeteer')
 const fs = require('fs');
 
 module.exports = render = async ( data, name, id, type ) => {
-    puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
-        .then(async browser => {
-            const page = await browser.newPage();
-            await fs.writeFile('./data/record/' + name + '.json', JSON.stringify(data), ()=>{});
+    const page = await browser.newPage();
+    await fs.writeFile('./data/record/' + name + '.json', JSON.stringify(data), ()=>{});
 
-            await page.goto('http://localhost:9934/src/views/' + name + '.html');
-            const htmlElement = await page.$('body');
-            const base64 = await htmlElement.screenshot({
-                encoding: 'base64'
-            });
+    await page.goto('http://localhost:9934/src/views/' + name + '.html');
+    const htmlElement = await page.$('body');
+    const base64 = await htmlElement.screenshot({
+        encoding: 'base64'
+    });
 
-            await browser.close();
-            await bot.sendMessage(id, "[CQ:image,file=base64://" + base64 + "]", type);
-        });
+    await page.close();
+    await bot.sendMessage(id, "[CQ:image,file=base64://" + base64 + "]", type);
 }
