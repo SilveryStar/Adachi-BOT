a83d08c198423450257eb94612880d6e133687e9
SilveryStar <1104247794@qq.com>
2021-06-26 20:23:43 +0800

    feat: add login script

---
+++ src/login.js
@@ -0,0 +1,27 @@
+const { readFileSync } = require( "fs" );
+const { parse } = require( "yaml" );
+const { createClient } = require( "oicq" );
+const { resolve } = require( "path" );
+
+( function() {
+	const file = readFileSync( resolve( __dirname, "../config/setting.yml" ), "utf-8" );
+	const setting = parse( file );
+	const bot = createClient( setting.number );
+	
+	// 处理登录滑动验证码
+	bot.on( "system.login.slider", () => {
+		process.stdin.once( "data", ( input ) => {
+			bot.sliderLogin( input.toString() );
+		} );
+	} );
+	
+	// 处理设备锁事件
+	bot.on( "system.login.device", () => {
+		bot.logger.info( "手机扫码完成后按下 Enter 继续..." );
+		process.stdin.once( "data", () => {
+			bot.login();
+		} );
+	} );
+	
+	bot.login( setting.password );
+} )();
