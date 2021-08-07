6c7cd8e5a64cbeeb73611364e5f9ae0ab584e042
He Yang <1160386205@qq.com>
2021-08-08 03:32:04 +0800

    fix: add an arc img at the right bottom of charbox

--- src/plugins/genshin/components/abyss-char-box.js
+++ src/plugins/genshin/components/abyss-char-box.js
@@ -1,6 +1,7 @@
 const template = `
 <div class="abyss-char-box">
     <img class="rarity-background" :src="rarityBackground" alt="ERROR"/>
+	<img class="corner" :src="cornerImg" alt="ERROR"/>
     <img class="avatar" :src="char.avatar_icon" alt="ERROR"/>
     <span class="info"> {{ char.value }} 次 </span>
 </div>`;
@@ -17,9 +18,12 @@ export default Vue.defineComponent( {
 		const rarityBackground = Vue.computed( () => {
 			return "/public/images/abyss/rarity" + props.char.rarity + ".png";
 		} );
-		
+		const cornerImg = Vue.computed( () => {
+			return "/public/images/abyss/corner.png";
+		} );
 		return {
-			rarityBackground
+			rarityBackground,
+			cornerImg
 		}
 	}
 } );
