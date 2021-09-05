bfa75444c28299d60491454989b250eeb7b9ce94
SilveryStar <1104247794@qq.com>
2021-09-06 00:35:10 +0800

    feat: update miHoYoBBS ds algorithm

--- src/plugins/genshin/utils/api.ts
+++ src/plugins/genshin/utils/api.ts
@@ -4,35 +4,36 @@ import fetch from "node-fetch";
 import { parse } from "yaml";
 
 const __API = {
-	FETCH_ROLE_ID: "https://api-takumi.mihoyo.com/game_record/card/wapi/getGameRecordCard",
-	FETCH_ROLE_INDEX: "https://api-takumi.mihoyo.com/game_record/genshin/api/index",
-	FETCH_ROLE_CHARACTERS: "https://api-takumi.mihoyo.com/game_record/genshin/api/character",
+	FETCH_ROLE_ID: "https://api-takumi.mihoyo.com/game_record/app/card/wapi/getGameRecordCard",
+	FETCH_ROLE_INDEX: "https://api-takumi.mihoyo.com/game_record/app/genshin/api/index",
+	FETCH_ROLE_CHARACTERS: "https://api-takumi.mihoyo.com/game_record/app/genshin/api/character",
 	FETCH_GACHA_LIST: "https://webstatic.mihoyo.com/hk4e/gacha_info/cn_gf01/gacha/list.json",
 	FETCH_GACHA_DETAIL: "https://webstatic.mihoyo.com/hk4e/gacha_info/cn_gf01/$/zh-cn.json",
-	FETCH_ARTIFACT: "http://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/artifact/artifact.yml",
+	FETCH_ARTIFACT: "https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/artifact/artifact.yml",
 	FETCH_SLIP: "https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/slip/index.yml",
 	FETCH_WISH_CONFIG: "https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/wish/config/$.json",
-	FETCH_INFO: "http://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/info/docs/$.json",
+	FETCH_INFO: "https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/info/docs/$.json",
 };
 
 const HEADERS = {
-	"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.7.0",
+	"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.11.1",
 	"Referer": "https://webstatic.mihoyo.com/",
-	"x-rpc-app_version": "2.7.0",
+	"x-rpc-app_version": "2.11.1",
 	"x-rpc-client_type": 5,
 	"DS": "",
 	"Cookie": ""
 };
 
 async function getBaseInfo( mysID: number, cookie: string ): Promise<any> {
+	const query = { uid: mysID };
 	return new Promise( ( resolve, reject ) => {
 		request( {
 			method: "GET",
 			url: __API.FETCH_ROLE_ID,
-			qs: { uid: mysID },
+			qs: query,
 			headers: {
 				...HEADERS,
-				"DS": getDS(),
+				"DS": getDS( query ),
 				"Cookie": cookie
 			}
 		} )
@@ -46,17 +47,18 @@ async function getBaseInfo( mysID: number, cookie: string ): Promise<any> {
 }
 
 async function getDetailInfo( uid: number, server: string, cookie: string ): Promise<any> {
+	const query = {
+		role_id: uid,
+		server
+	};
 	return new Promise( ( resolve, reject ) => {
 		request( {
 			method: "GET",
 			url: __API.FETCH_ROLE_INDEX,
-			qs: {
-				server,
-				role_id: uid
-			},
+			qs: query,
 			headers: {
 				...HEADERS,
-				"DS": getDS(),
+				"DS": getDS( query ),
 				"Cookie": cookie
 			}
 		} )
@@ -70,19 +72,21 @@ async function getDetailInfo( uid: number, server: string, cookie: string ): Pro
 }
 
 async function getCharactersInfo( roleID: number, server: string, charIDs: number[], cookie: string ): Promise<any> {
+	const body = {
+		character_ids: charIDs,
+		role_id: roleID,
+		server
+	};
+	
 	return new Promise( ( resolve, reject ) => {
 		request( {
 			method: "POST",
 			url: __API.FETCH_ROLE_CHARACTERS,
 			json: true,
-			body: {
-				server,
-				character_ids: charIDs,
-				role_id: roleID
-			},
+			body,
 			headers: {
 				...HEADERS,
-				"DS": getDS(),
+				"DS": getDS( undefined, JSON.stringify( body ) ),
 				"Cookie": cookie,
 				"content-type": "application/json"
 			}
--- src/plugins/genshin/utils/ds.ts
+++ src/plugins/genshin/utils/ds.ts
@@ -1,11 +1,23 @@
 import { Md5 } from "md5-typescript";
 import { randomString } from "./random";
 
-function calculate() {
-	const n: string = "14bmu1mz0yuljprsfgpvjh3ju2ni468r";
+function getQueryParam( data: any ): string {
+	if ( data === undefined ) {
+		return "";
+	}
+	const arr: string[] = [];
+	for ( let key of Object.keys( data ) ) {
+		arr.push( `${ key }=${ data[key] }` );
+	}
+	return arr.join( "&" );
+}
+
+function calculate( query: any, body: string = "" ): string {
+	const n: string = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
 	const i: number = Date.now() / 1000 | 0;
 	const r: string = randomString( 6 );
-	const c: string = Md5.init( `salt=${ n }&t=${ i }&r=${ r }` );
+	const q: string = getQueryParam( query );
+	const c: string = Md5.init( `salt=${ n }&t=${ i }&r=${ r }&b=${ body }&q=${ q }` );
 	
 	return `${ i },${ r },${ c }`;
 }
