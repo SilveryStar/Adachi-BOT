4879837350c9b76585e5443abf5ac65d24e671d5
SilveryStar <1104247794@qq.com>
2021-09-14 10:52:26 +0800

    fix: info query error when UID exceeds 200M

--- src/plugins/genshin/achieves/uid-query.ts
+++ src/plugins/genshin/achieves/uid-query.ts
@@ -3,12 +3,22 @@ import { characterInfoPromise, detailInfoPromise } from "../utils/promise";
 import { Redis } from "../../../bot";
 import { render } from "../utils/render";
 
-function getID( data: string ): [ number, string ] | string {
-	if ( data.length !== 9 || ( data[0] !== "1" && data[0] !== "5" ) ) {
+function getRegion( first: string ): string {
+	switch ( first ) {
+		case "1": return "cn_gf01";
+		case "2": return "cn_gf01";
+		case "5": return "cn_qd01";
+		default:  return "unknown";
+	}
+}
+
+function getUserInfo( data: string ): [ number, string ] | string {
+	const reg = new RegExp( /^[125][0-9]{8}$/g );
+	if ( !reg.test( data ) ) {
 		return "输入 UID 不合法";
 	}
 	const uid: number = parseInt( data );
-	const region: string = data[0] === "1" ? "cn_gf01" : "cn_qd01";
+	const region: string = getRegion( data[0] );
 	
 	return [ uid, region ];
 }
@@ -18,7 +28,7 @@ type Message = GroupMessageEventData | PrivateMessageEventData;
 async function main( sendMessage: ( content: string ) => any, message: Message ): Promise<void> {
 	const data: string = message.raw_message;
 	const qqID: number = message.user_id;
-	const info: [ number, string ] | string = getID( data );
+	const info: [ number, string ] | string = getUserInfo( data );
 	
 	if ( typeof info === "string" ) {
 		await sendMessage( info );
