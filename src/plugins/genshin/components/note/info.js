d046addeb9fd7e98e6a5ea6fa1d3985ce3999286
MarryDream <2037177696@qq.com>
2022-02-01 02:28:35 +0800

    style: add icon to note page

--- src/plugins/genshin/components/note/info.js
+++ src/plugins/genshin/components/note/info.js
@@ -1,5 +1,5 @@
-const template =
-`<div class="info box">
+const template = `<div class="info box">
+	<img :src="data.icon" alt="ERROR" />
 	<div class="text">
 		<p class="info-title">{{ data.title }}</p>
 		<p class="info-subtitle">{{ data.subtitle }}</p>
