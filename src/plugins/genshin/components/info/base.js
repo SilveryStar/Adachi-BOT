a7824ebc2bc64a55a39ad86280a1d062a6b9d10c
SilveryStar <1104247794@qq.com>
2022-03-01 21:56:15 +0800

    feat: add artifact information page

--- src/plugins/genshin/components/info/base.js
+++ src/plugins/genshin/components/info/base.js
@@ -1,9 +1,11 @@
 const template = `<div class="info-base" :class="infoBackgroundClass">
-	<img :class="type === '角色' ? 'character' : 'weapon'" :src="mainImage" alt="ERROR"/>
+	<img :class="infoType" :src="mainImage" alt="ERROR"/>
 	<div class="base-content">
 		<nav class="base-info-title">
-        	<p class="title-and-name">「{{ title }}·{{ name }}」</p>
-        	<p class="introduce">{{ introduce }}</p>
+        	<p class="title-and-name">
+        		「<span v-show="infoType !== 'artifact'">{{ title }}·</span>{{ name }}」
+        	</p>
+        	<p class="introduce" v-html="introduce"></p>
 		</nav>
 		<main class="base-info-content">
 			<slot></slot>
@@ -33,18 +35,36 @@ export default defineComponent( {
 	setup( props ) {
 		const data = props.data;
 		
+		if ( data.type === "圣遗物" ) {
+			data.introduce =
+				'<span class="access-info title">获取途径:</span>' +
+				`<span class="access-info content">${ data.access }</span>`;
+		}
+		
 		const infoBackgroundClass = computed( () => {
 			return `rarity_${ data.rarity }`;
 		} );
+		const infoType = computed( () => {
+			switch ( data.type ) {
+				case "角色": return "character";
+				case "武器": return "weapon";
+				case "圣遗物": return "artifact";
+			}
+		} );
 		const mainImage = computed( () => {
-			const link = data.type === "角色" ? `character/${ data.id }.png` : `weapon/${ data.name }.png`;
-			return `https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/${ link }`;
+			const baseURL = "https://adachi-bot.oss-cn-beijing.aliyuncs.com/Version2/";
+			switch ( data.type ) {
+				case "角色": return baseURL + `character/${ data.id }.png`;
+				case "武器": return baseURL + `weapon/${ data.name }.png`;
+				case "圣遗物": return baseURL + `artifact/${ data.id }/${ data.icon }.png`;
+			}
 		} );
 		
 		return {
 			...toRefs( data ),
 			infoBackgroundClass,
-			mainImage
+			mainImage,
+			infoType
 		}
 	}
 } );
