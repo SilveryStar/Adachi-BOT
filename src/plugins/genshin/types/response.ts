5deea90fa9ba8059cd342912c2a22a71cca0e25a
SilveryStar <1104247794@qq.com>
2022-03-20 01:07:52 +0800

    feat: add ledger api and command

--- src/plugins/genshin/types/response.ts
+++ src/plugins/genshin/types/response.ts
@@ -3,10 +3,11 @@ import { BBS } from "./hoyobbs";
 import { Character } from "./character";
 import { UserInfo } from "./user-info";
 import { Note } from "./note";
+import { Ledger } from "./ledger";
 import { SignInInfo, SignInResult } from "#genshin/types/sign-in";
 
 export type ResponseDataType = Abyss | BBS | Character |
-	UserInfo | Note | SignInInfo | SignInResult;
+	UserInfo | Note | SignInInfo | SignInResult | Ledger
 
 export interface ResponseBody {
 	retcode: number;
