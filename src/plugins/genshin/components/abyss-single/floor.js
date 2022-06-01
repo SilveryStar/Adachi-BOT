6fc28c5dece9d5f7e25b88f601b64075e614de29
MarryDream <2037177696@qq.com>
2022-06-01 21:48:25 +0800

    fix: Optimize the style of the abyss-single page when the data is incomplete, Ref #246

--- src/plugins/genshin/components/abyss-single/floor.js
+++ src/plugins/genshin/components/abyss-single/floor.js
@@ -1,13 +1,13 @@
 const template = `<div class="floor">
 	<header class="floor-title">第{{ data.index }}层</header>
 	<section class="floor-room-list">
-		<Room v-for="(r, rKey) of data.levels" :key="rKey" :data="r"></Room>
+		<Room v-for="(l, lKey) of levels" :key="lKey" :data="l"></Room>
 	</section>
 </div>`;
 
 import Room from "./room.js"
 
-const { defineComponent } = Vue;
+const { defineComponent, computed } = Vue;
 
 export default defineComponent( {
 	name: "Floor",
@@ -17,8 +17,27 @@ export default defineComponent( {
 	props: {
 		data: {
 			type: Object,
-			default: () => ( {} )
+			default: () => ( {
+				index: 0,
+				levels: []
+			} )
 		}
 	},
-	template
+	template,
+	setup(props) {
+		const data = props.data;
+
+		/* 获取三间数据，无数据使用默认数据填充 */
+		const levels = new Array(3).fill('').map((fake, lKey) => {
+			const index = lKey + 1;
+			const level = data.levels?.find(f => f.index === index);
+			return level || {
+				index,
+				battles: []
+			}
+		})
+		return {
+			levels
+		}
+	}
 } )
