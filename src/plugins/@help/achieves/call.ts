95fa7ae37526c376d261be9896697f8e99c8da47
MarryDream <2037177696@qq.com>
2022-07-16 00:03:36 +0800

    feat: add command call: send massage to the bot's owner

---
+++ src/plugins/@help/achieves/call.ts
@@ -0,0 +1,36 @@
+import { InputParameter } from "@modules/command/main";
+
+export async function main( { sendMessage, messageData, message, redis, config }: InputParameter ): Promise<void> {
+	const user = messageData.user_id;
+	const content = messageData.raw_message;
+	
+	const noSupportMsg = messageData.message.find( m => ![ "text", "image" ].includes( m.type ) )
+	if ( noSupportMsg ) {
+		await sendMessage( "仅允许发送包含文字/图片的内容" );
+		return;
+	}
+	
+	const dbKey = `adachi.call-limit-${ user }`;
+	let limit: number = parseInt( await redis.getString( dbKey ) );
+	
+	if ( limit <= 0 ) {
+		await sendMessage( "今日已反馈次数已用尽" )
+		return;
+	}
+	
+	if ( isNaN( limit ) ) {
+		limit = config.callTimes;
+	}
+	
+	limit = ( isNaN( limit ) ? config.callTimes : limit ) - 1;
+	
+	await message.sendMaster( `来自用户「${ user }」：${ content }` );
+	await sendMessage( `发送成功，今日剩余反馈次数：${ limit }` );
+	
+	/* 获取过期时间 */
+	const todayEndTime = new Date( new Date().toLocaleDateString() ).getTime() + 24 * 60 * 60 * 1000;
+	const keyTimeOut = Math.ceil( ( todayEndTime - new Date().getTime() ) / 1000 );
+	
+	await redis.setString( dbKey, limit );
+	await redis.setTimeout( dbKey, keyTimeOut );
+}
