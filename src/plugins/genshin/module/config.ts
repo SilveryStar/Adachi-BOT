76200e7c4625174ffd0abd2a417ea1ce5e3cb11a
Extrwave <exwave@foxmail.com>
2023-01-17 15:37:07 +0800

    development：support verify，wait to test

--- src/plugins/genshin/module/config.ts
+++ src/plugins/genshin/module/config.ts
@@ -1,4 +1,5 @@
 import { RefreshCatch } from "@modules/management/refresh";
+import { randomString } from "#genshin/utils/random";
 
 export default class GenshinConfig {
 	public cardWeaponStyle: "normal" | "weaponA" | "weaponB";
@@ -6,13 +7,23 @@ export default class GenshinConfig {
 	public serverPort: number;
 	public showCharScore: boolean | number[];
 	public wishLimitNum: number;
+	public verifyEnable: boolean;
+	public verifyRepeat: number;
+	public verifyToken: string;
 	
 	public static init = {
+		tip: "verify相关:\n" +
+			"verifyEnable: 验证码服务总开关，true开启，false关闭\n" +
+			"verifyRepeat: 验证码有概率失败，重试次数，推荐设置 1\n" +
+			"verifyToken: 验证码服务Token，前往 https://challenge.minigg.cn/ 购买",
 		cardWeaponStyle: "normal",
 		cardProfile: "random",
 		serverPort: 58612,
 		showCharScore: true,
-		wishLimitNum: 99
+		wishLimitNum: 99,
+		verifyEnable: false,
+		verifyRepeat: 1,
+		verifyToken: randomString( 6 )
 	};
 	
 	constructor( config: any ) {
@@ -21,6 +32,9 @@ export default class GenshinConfig {
 		this.serverPort = config.serverPort;
 		this.showCharScore = config.showCharScore;
 		this.wishLimitNum = config.wishLimitNum;
+		this.verifyEnable = config.verifyEnable;
+		this.verifyRepeat = config.verifyRepeat;
+		this.verifyToken = config.verifyToken;
 	}
 	
 	public async refresh( config ): Promise<string> {
@@ -29,9 +43,12 @@ export default class GenshinConfig {
 			this.cardProfile = config.cardProfile;
 			this.showCharScore = config.showCharScore;
 			this.wishLimitNum = config.wishLimitNum;
+			this.verifyEnable = config.verifyEnable;
+			this.verifyRepeat = config.verifyRepeat;
+			this.verifyToken = config.verifyToken;
 			return "genshin.yml 重新加载完毕";
 		} catch ( error ) {
-			throw <RefreshCatch> {
+			throw <RefreshCatch>{
 				log: ( <Error>error ).stack,
 				msg: "genshin.yml 重新加载失败，请前往控制台查看日志"
 			};
--- src/plugins/genshin/types/response.ts
+++ src/plugins/genshin/types/response.ts
@@ -7,10 +7,11 @@ import { Ledger } from "./ledger";
 import { SignInInfo, SignInResult } from "#genshin/types/sign-in";
 import { AvatarDetailRaw } from "#genshin/types/avatar";
 import { CalendarList, CalendarDetail } from "#genshin/types/calendar";
+import { VerifyError } from "#genshin/types/verify-code";
 
 export type ResponseDataType = Abyss | BBS | Character |
 	UserInfo | Note | SignInInfo | SignInResult | Ledger | AvatarDetailRaw |
-	CalendarList | CalendarDetail;
+	CalendarList | CalendarDetail | VerifyError;
 
 export interface ResponseBody {
 	retcode: number;
